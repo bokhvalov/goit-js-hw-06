6e892d0afc96b9b1bcac876ef25ef0c7b714ec73
Ivan Bokhvalov <bvancha@gmail.com>
2022-08-24 14:51:55 +0200

    10 task update

--- js/task-10.js
+++ js/task-10.js
@@ -2,33 +2,35 @@ const createBtn = document.querySelector("button[data-create]");
 const destroyBtn = document.querySelector("button[data-destroy]");
 const boxes = document.querySelector("#boxes");
 
-createBtn.addEventListener ("click",createBoxes);
-destroyBtn.addEventListener ("click",eraseBoxes);
-
+createBtn.addEventListener("click", createBoxes);
+destroyBtn.addEventListener("click", eraseBoxes);
 
 let boxWidth = 30;
 let boxHeight = 30;
 
 function createBoxes() {
   const inputVal = document.querySelector("input").value;
-console.log(inputVal);
-for (let i=0; i<inputVal; i+=1){
-const container = document.createElement("div");
-container.style.width = `${boxWidth}px`;
-container.style.height = `${boxHeight}px`;
-container.style.backgroundColor= getRandomHexColor();
+  console.log(inputVal);
+  for (let i = 0; i < inputVal; i += 1) {
+    const container = document.createElement("div");
 
-boxWidth = boxWidth+10;
-boxHeight = boxHeight+10;
+    container.style.width = `${boxWidth}px`;
+    container.style.height = `${boxHeight}px`;
+    container.style.backgroundColor = getRandomHexColor();
 
-boxes.insertAdjacentElement("beforeend", container);
+    boxWidth = boxWidth + 10;
+    boxHeight = boxHeight + 10;
+
+    boxes.insertAdjacentElement("beforeend", container);
+  }
 }
-};
-
 
 function eraseBoxes() {
-  boxes.innerHTML = '';
+  boxes.innerHTML = "";
+  boxWidth = 30;
+  boxHeight = 30;
 }
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
